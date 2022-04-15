571c00bc3622607da702f3183231dabcaf22ba2f
Karima-Aktari <karimaaktari911@gmail.com>
2022-04-15 21:08:23 +0600

    commit about

--- src/Components/Home/AboutUs/AboutUs.js
+++ src/Components/Home/AboutUs/AboutUs.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const AboutUs = () => {
     return (
-        <div>
-            <h3 className='text-blue-500'>About Us</h3>
-            <div>
+        <div className='mx-auto py-7'>
 
-            </div>
-            <div>
+            <div className='grid md:grid-cols-2 sm:grid-cols-1'>
+                <div className=' relative'>
+                    <img src='https://images.freeimages.com/images/small-previews/0c1/house-on-a-farm-1221758.jpg' className="w-5/6 pl-10 rounded-r-lg" alt='' />
+                    <div className='absolute bg-blue-100 bg-opacity-40 absolute h-full w-5/6 -left-7 -top-7 rounded-r-lg'>
+                    </div>
+                </div>
+                <div className='w-full text-left'>
+                    <h3 className='text-blue-500 font-medium'>About Us</h3>
+                    <h1 className='text-3xl font-bold'>We Are Providing The Top</h1>
+                    <h1 className='text-3xl font-bold'><span className='text-blue-500'>Real State </span>Property</h1>
+                    <p className='text-md text-gray-700 py-5 flex flex-wrap font-medium'>
+                        Find Digital Agencies In Usa! Always Facts. Results & Answers. Privacy Friendly. Unlimited Access. The Best Resources. Types: Best Results, Explore Now, New Sources.   Find Digital Agencies In Usa! Always Facts. Results & Answers. Privacy Friendly. Unlimited Access. The Best Resources. Types: Best Results, Explore Now, New Sources.
+                    </p>
+                    <p className='text-md text-gray-700 py-5 flex flex-wrap font-medium'>
+                        Find Digital Agencies In Usa! Always Facts. Results & Answers. Privacy Friendly. Unlimited Access. The Best Resources. Types: Best Results, Explore Now, New Sources.
+                    </p>
+                    <NavLink as={Link} to="/home">
+                        <button className='bg-blue-600 px-5 py-1 text-lg text-slate-200 font-medium border-2 border-blue-600'>Learn More</button>
+                    </NavLink>
+                    <NavLink as={Link} to="/home">
+                        <button className='px-5 py-1 text-lg text-blue-500 font-medium border-2 border-blue-600 mx-2'>Contact Us</button>
+                    </NavLink>
+
+                </div>
             </div>
         </div>
     );
--- src/Components/Home/Brands/Brands.js
+++ src/Components/Home/Brands/Brands.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Brands = () => {
     return (
-        <div className='container mx-auto my-7 mt-10'>
+        <div className='container mx-auto my-12 mt-10'>
 
-            <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 text-black gap-4'>
+            <div className='grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1 text-black gap-4 px-4'>
 
-                <div className='p-2 transition hover:-translate-y-2 hover:scale-110 hover:shadow duration-300'>
+                <div className='p-2 transition hover:-translate-y-2 hover:scale-110 hover:shadow duration-300  sm:transform-none'>
                     <img src="https://template.annimexweb.com/diva/assets/images/logo/brandlogo1.png" className='w-full' alt='' />
                 </div>
 
