efbcbf241470e1a070f63d8025cc5677d4e84990
Karima-Aktari <karimaaktari911@gmail.com>
2022-04-16 11:27:39 +0600

    trading properties

--- src/Components/Home/TradingProperties/TradingProperties.js
+++ src/Components/Home/TradingProperties/TradingProperties.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import heart from '../../../images/Icons/heart-icon.png';
+import share from '../../../images/Icons/share-icon.png';
+import bed from '../../../images/Icons/bed.png';
+import bath from '../../../images/Icons/bath.png';
+import square from '../../../images/Icons/square.png';
 
 const TradingProperties = () => {
 
@@ -43,16 +48,43 @@ const TradingProperties = () => {
                     </p>
                 </div>
             </div>
-            <div className='grid grid-cols-3 gap-5'>
-                {
-                    properties.map((property) => (
+            <div>
+                <div className='grid md:grid-cols-3 sm:grid-cols-1 gap-5'>
+                    {
+                        properties.map((property) => (
 
-                        <div className="p-4">
-                            <img src={property.img} className="rounded" alt='' />
-                            <h1>${property.price}</h1>
-                            <h2>{property.title}</h2>
-                        </div>
-                    ))}
+                            <div className="p-4">
+                                <div className='relative'>
+                                    <img src={property.img} className="rounded-3xl" alt='' />
+                                    <h4 className='absolute top-4 left-4 px-2 font-bold rounded-full bg-slate-50 text-blue-500'>Available Now</h4>
+                                </div>
+                                <div className='flex justify-between py-2'>
+                                    <h1 className='text-md font-bold text-blue-500'>${property.price} </h1>
+                                    <div className='flex justify-end w-40'>
+                                        <img src={heart} className="w-6 h-6 mx-3" alt='' />
+                                        <img src={share} className="w-6 h-6" alt='' />
+                                    </div>
+                                </div>
+                                <h2 className='text-xl font-bold'>{property.title}</h2>
+                                <p className='text-gray-500 py-2'>{property.location}</p>
+                                <div className='flex justify-start text-sm'>
+                                    <div className='flex justify-between'>
+                                        <img src={bed} className="w-5 h-5" alt='' />
+                                        <p className='mx-3'>{property.bed} Bd</p>
+                                    </div>
+                                    <div className='flex justify-between  mx-4'>
+                                        <img src={bath} className="w-5 h-5" alt='' />
+                                        <p className='mx-3'>{property.bath} Bth</p>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <img src={square} className="w-5 h-5" alt='' />
+                                        <p className='mx-3'>{property.square} Sqft</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))}
+                </div>
             </div>
 
         </div>
